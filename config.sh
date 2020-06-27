8f9fe5737b905943ef96157fae044d49065b9071
Alex Nuclear Boy <alex.nuclearboy@gmail.com>
2020-06-27 13:48:13 +0200

    add script to create directories and copy files

---
+++ config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G20_Bs10.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G20_Bs20.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G20_Bs30.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G20_Bs40.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G30_Bs20.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G30_Bs30.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G30_Bs40.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G40_Bs20.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G40_Bs30.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G40_Bs40.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G50_Bs10.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G50_Bs20.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G50_Bs30.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G50_Bs40.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_G150_Bs20.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-bound-pdpi0_ProtDist.root ./input
+scp wasa:/data7/users/khreptak/OUTPUT/MC/MC-newcuts-AddGammaCut-pd-pdpi0.root ./input
+mkdir ./output
+mkdir ./output/plots
+mkdir ./output/files
